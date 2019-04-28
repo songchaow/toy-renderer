96eab8af71fabec790cbd675777057e7ff265367
Songchao Wang <songchaow@outlook.com>
2019-04-28 23:03:06 +0800

    Add Quadratic.

--- toy_tracer/utils/utils.h
+++ toy_tracer/utils/utils.h
@@ -16,3 +16,21 @@ inline T Clamp(T val, U low, V high) {
     else
         return val;
 }
+
+inline bool Quadratic(Float a, Float b, Float c, Float *t0, Float *t1) {
+      // Find quadratic discriminant
+      double discrim = (double)b * (double)b - 4 * (double)a * (double)c;
+      if (discrim < 0) return false;
+      double rootDiscrim = std::sqrt(discrim);
+
+      // Compute quadratic _t_ values
+      double q;
+      if (b < 0)
+            q = -.5 * (b - rootDiscrim);
+      else
+            q = -.5 * (b + rootDiscrim);
+      *t0 = q / a;
+      *t1 = c / q;
+      if (*t0 > *t1) std::swap(*t0, *t1);
+      return true;
+}
