7eeb6e921bcb1935aa1066f9b8d212fb0ff2eb6f
Songchao Wang <songchaow@outlook.com>
2020-01-24 14:39:53 +0800

    Edit o2cam.

--- toy_tracer/core/cubemap.cpp
+++ toy_tracer/core/cubemap.cpp
@@ -5,12 +5,12 @@
 
 /*static*/ const Transform CubeMap::camtoNDC { toNDCPerspective(0.1f, 1000.f, 1.f, 90.f/ 180.f * Pi) };
 /*static*/ const Transform CubeMap::o2cam[6] = { 
-      LookAt(Point3f(0.f, 0.f, 0.f), Vector3f(1.f, 0.f, 0.f)),                            // X+
-      LookAt(Point3f(0.f, 0.f, 0.f), Vector3f(-1.f, 0.f, 0.f)),                           // X-
-      LookAt(Point3f(0.f, 0.f ,0.f), Vector3f(0.f, 1.f, 0.f), Vector3f(0.f, 0.f ,-1.f)),  // Y+
-      LookAt(Point3f(0.f, 0.f, 0.f), Vector3f(0.f, -1.f, 0.f), Vector3f(0.f, 0.f, 1.f)),  // Y-
-      LookAt(Point3f(0.f, 0.f, 0.f), Vector3f(0.f, 0.f, 1.f), Vector3f(0.f, 1.f, 0.f)),   // Z+
-      LookAt(Point3f(0.f, 0.f ,0.f), Vector3f(0.f, 0.f, -1.f), Vector3f(0.f, 1.f, 0.f))    // Z-
+      LookAt(Point3f(0.f, 0.f, 0.f), Vector3f(1.f, 0.f, 0.f), Vector3f(0.f, -1.f, 0.f)),                            // X+
+      LookAt(Point3f(0.f, 0.f, 0.f), Vector3f(-1.f, 0.f, 0.f), Vector3f(0.f, -1.f, 0.f)),                           // X-
+      LookAt(Point3f(0.f, 0.f ,0.f), Vector3f(0.f, 1.f, 0.f), Vector3f(0.f, 0.f ,1.f)),  // Y+
+      LookAt(Point3f(0.f, 0.f, 0.f), Vector3f(0.f, -1.f, 0.f), Vector3f(0.f, 0.f, -1.f)),  // Y-
+      LookAt(Point3f(0.f, 0.f, 0.f), Vector3f(0.f, 0.f, 1.f), Vector3f(0.f, -1.f, 0.f)),   // Z+
+      LookAt(Point3f(0.f, 0.f ,0.f), Vector3f(0.f, 0.f, -1.f), Vector3f(0.f, -1.f, 0.f))    // Z-
 };
 
 CubeMap::CubeMap(const Point3f& o) : o(o) {
